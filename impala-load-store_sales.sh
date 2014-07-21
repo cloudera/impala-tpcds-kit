2cf53197a6997737307d0f8c3d46ffa2ab190906
ishaan <ishaan@cloudera.com>
2014-07-21 16:36:15 -0700

    Dynamically generate the queries to load a partitioned store_sales table.

--- impala-load-store_sales.sh
+++ impala-load-store_sales.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source tpcds-env.sh
 
-impala-shell -d $TPCDS_DBNAME <<EOF
+impala-shell -d ${TPCDS_DBNAME} -q "
 drop table if exists store_sales;
 
 create table store_sales
@@ -30,34 +30,6 @@ create table store_sales
   ss_net_profit double
 )
 partitioned by (ss_sold_date_sk int)
-stored as parquetfile
-;
+stored as parquetfile"
 
-insert overwrite table store_sales
-partition(ss_sold_date_sk) [shuffle]
-select
-ss_sold_time_sk,
-ss_item_sk,
-ss_customer_sk,
-ss_cdemo_sk,
-ss_hdemo_sk,
-ss_addr_sk,
-ss_store_sk,
-ss_promo_sk,
-ss_ticket_number,
-ss_quantity,
-ss_wholesale_cost,
-ss_list_price,
-ss_sales_price,
-ss_ext_discount_amt,
-ss_ext_sales_price,
-ss_ext_wholesale_cost,
-ss_ext_list_price,
-ss_ext_tax,
-ss_coupon_amt,
-ss_net_paid,
-ss_net_paid_inc_tax,
-ss_net_profit,
-ss_sold_date_sk
-from et_store_sales;
-EOF
+python load-store-sales.py
