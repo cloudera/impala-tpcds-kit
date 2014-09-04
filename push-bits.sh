fadd3afc8ffb71315e15b50d84932309ea6ec9cb
Sohan Jain <sjain@localytics.com>
2014-09-04 10:57:39 -0400

    run make for tpcds dsdgen on cluster nodes

--- push-bits.sh
+++ push-bits.sh
@@ -7,6 +7,8 @@ source tpcds-env.sh
 
 cat dn.txt | while read h
 do
-  scp -rp "$HOMEDIR/$TPCDS_DIR" $CLUSTER_USER@$h:$CLUSTER_HOMEDIR
+  #scp -rp "$HOMEDIR/$TPCDS_DIR" $CLUSTER_USER@$h:$CLUSTER_HOMEDIR
   scp -rp "$HOMEDIR/$MPP_TPCDS_DIR" $CLUSTER_USER@$h:$CLUSTER_HOMEDIR
+  ssh $CLUSTER_USER@$h "./$CLUSTER_HOMEDIR/$MPP_TPCDS_DIR/init-tpcds.sh"
 done
+wait
