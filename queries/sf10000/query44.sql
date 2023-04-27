689576938e097d595fddcbcc0e69c30a4b47b31a
David Rorke <drorke@cloudera.com>
2023-04-26 17:58:30 -0700

    Fix hardcoded value for ss_store_sk predicate in query 44 to use value that actually exists in the store sales table. Predicate value generated by dsqgen didn't exist in store_sales (bug in dsqgen) which resulted in empty result set.
    Fixing just sf3000 and sf10000 queries for now. Queries for other scale factors will
    be fixed in a future commit.

--- queries/sf10000/query44.sql
+++ queries/sf10000/query44.sql
@@ -4,11 +4,11 @@ from(select *
      from (select item_sk,rank() over (order by rank_col asc) rnk
            from (select ss_item_sk item_sk,avg(ss_net_profit) rank_col 
                  from store_sales ss1
-                 where ss_store_sk = 366
+                 where ss_store_sk = 457
                  group by ss_item_sk
                  having avg(ss_net_profit) > 0.9*(select avg(ss_net_profit) rank_col
                                                   from store_sales
-                                                  where ss_store_sk = 366
+                                                  where ss_store_sk = 457
                                                     and ss_cdemo_sk is null
                                                   group by ss_store_sk))V1)V11
      where rnk  < 11) asceding,
@@ -16,11 +16,11 @@ from(select *
      from (select item_sk,rank() over (order by rank_col desc) rnk
            from (select ss_item_sk item_sk,avg(ss_net_profit) rank_col
                  from store_sales ss1
-                 where ss_store_sk = 366
+                 where ss_store_sk = 457
                  group by ss_item_sk
                  having avg(ss_net_profit) > 0.9*(select avg(ss_net_profit) rank_col
                                                   from store_sales
-                                                  where ss_store_sk = 366
+                                                  where ss_store_sk = 457
                                                     and ss_cdemo_sk is null
                                                   group by ss_store_sk))V2)V21
      where rnk  < 11) descending,
